341708ac8773d206465e328fc167f4b9d1df0317
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-09-07 10:47:58 -0400

    First C++ program

---
+++ ZAA/01-Sep07/prg.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+//#include <cstdlib> how to include C header files
+using namespace std;
+int main( ) {
+   cout << "Hello OOP244ZAA..." << endl;
+   printf( "And hello again...\n" );
+   return 0;
+}
