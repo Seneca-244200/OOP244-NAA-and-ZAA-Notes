000751ed29db22cfcbc9e4661aadc945ee466153
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-10-04 11:16:22 -0400

    O8-Oct 02

---
+++ ZAA/08-Oct02/a-shyCin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+   cout << "OOP244 ZAA 08-Oct02" << endl;
+   int num;
+   //int a, b, c, d;
+   //cin >> a >> b >> c >> d;
+   for ( int i = 0; i < 4; i++ ) {
+      cout << "Enter an int\n> ";
+      cin >> num;
+      if ( cin ) {
+         cout << "read ok!" << endl;
+      }
+      else {
+         cout << "last read failed!" << endl;
+      }
+      cout << "read: " << num << endl;
+   }
+   return 0;
+}
