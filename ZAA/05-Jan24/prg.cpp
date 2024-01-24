3e8d6706e7a8ee80d84bfaefe2852d7f62610801
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-01-24 10:43:49 -0500

    Encapsulation step 1

--- ZAA/05-Jan24/prg.cpp
+++ ZAA/05-Jan24/prg.cpp
@@ -7,82 +7,69 @@ using namespace std;
 struct Name {
    char* m_name;
    char* m_surname;
-};
-void foo( ) {
-   int a{ };
-   int b; 
-   b = 20;
-
-
-
-}
-/// <summary>
+   /// <summary>
 /// sets the name and surname dynamically in the Name object
 /// </summary>
-/// <param name="n"> Name reference</param>
 /// <param name="name"> cstring fist name </param>
 /// <param name="surname"> cstring last name </param>
-void set( Name& n, const char* name, const char* surname ) {
-   if ( name && surname && name[0] && surname[0] ) {
-      n.m_name = new char[strlen(name )+1];
-      n.m_surname = new char[strlen(surname )+1];
-      strcpy( n.m_name, name );
-      strcpy( n.m_surname, surname );
+   void set( const char* name, const char* surname ) {
+      if ( name && surname && name[0] && surname[0] ) {
+         m_name = new char[strlen( name ) + 1];
+         m_surname = new char[strlen( surname ) + 1];
+         strcpy( m_name, name );
+         strcpy( m_surname, surname );
+      }
    }
-}
-
-/// <summary>
+   /// <summary>
 /// deallocates the memory used by Name object
 /// </summary>
 /// <param name="n"> The Name object</param>
-void deallocate( Name& n ) {
-   delete[] n.m_name;
-   delete[] n.m_surname;
-   n.m_name = n.m_surname = nullptr;
-}
-
-/// <summary>
+   void deallocate( ) {
+      delete[] m_name;
+      delete[] m_surname;
+      m_name = m_surname = nullptr;
+   }
+   /// <summary>
 /// set a name by getting them from console from user.
 /// </summary>
-/// <param name="N"> name object</param>
-void read( Name& N ) {
-   char* name= new char[64];
-   char* surname = new char[128];
-   cout << "Name: ";
-   cin >> name;
-   cout << "Surname: ";
-   cin >> surname;
-   set( N, name, surname );
-   delete[] name;
-   delete[] surname;
-}
-
-/// <summary>
+   void read() {
+      char* name = new char[64];
+      char* surname = new char[128];
+      cout << "Name: ";
+      cin >> name;
+      cout << "Surname: ";
+      cin >> surname;
+      set(name, surname );
+      delete[] name;
+      delete[] surname;
+   }
+   /// <summary>
 /// returns true if the name is empty
 /// </summary>
-/// <param name="N"> Name object</param>
 /// <returns> true if name is empty </returns>
-bool isEmpty( const Name& N ) {
-   return N.m_name == nullptr || N.m_surname == nullptr;
-}
+   bool isEmpty() const {
+      //m_name = new char[50]; not possible since the method is a constant one
+      return m_name == nullptr || m_surname == nullptr;
+   }
 
-
-void setEmpty( Name& n ) {
-   n.m_name = n.m_surname = nullptr;
-}
-
-/// <summary>
+   /// <summary>
+   /// sets the attributes of name to nullptr
+   /// </summary>
+   void setEmpty( ) {
+      m_name = m_surname = nullptr;
+   }
+   /// <summary>
 /// Print a name or prints "No Name" if empty
 /// </summary>
-/// <param name="N"> Name Object</param>
-void print( const Name& N ) {
-   if ( !isEmpty( N ) ) {
-      cout << N.m_name << " " << N.m_surname << endl;
+   void print( )const {
+      if ( !isEmpty() ) {
+         cout << m_name << " " << m_surname << endl;
+      }
+      else {
+         cout << "No Name!" << endl;
+      }
    }
-   else {
-      cout << "No Name!" << endl;
-   }
-}
+};
 
 
 int main( ) {
@@ -93,23 +80,25 @@ int main( ) {
    cin >> noOfNames;
    n = new Name[noOfNames];
    for ( size_t i = 0; i < noOfNames; i++) {
-      setEmpty( n[i] );
+      n[i].setEmpty(  );
    }
-
-   print( n[0] );
+   n[0].print( );
    if ( n ) {
       size_t i{};
       for ( ; i < noOfNames; i++ ) {
          cout << (i + 1) << "> ";
-         read(n[i]);
+         n[i].read();
       }
       for ( i = 0; i < noOfNames; i++ ) {
          cout << (i + 1) << ": ";
-         print( n[noOfNames - i - 1] );
+         n[noOfNames - i - 1].print( );
       }
    }
    else {
       cout << "Memory allocation failed!" << endl;
+   }
+   for ( size_t i = 0; i < noOfNames; i++ ) {
+      n[i].deallocate( );
    }
    delete[] n;
    n = nullptr;// not needed, just to force a habit!
